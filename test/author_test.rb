1d0fe3f58b674856b789d4f5b9e5fab20dfe5245
Jori Peterson <joripeterson@gmail.com>
2019-05-22 15:57:33 -0600

    Add unfinished write test

--- test/author_test.rb
+++ test/author_test.rb
@@ -16,6 +16,19 @@ class AuthorTest < Minitest::Test
     def test_it_has_attributes
       assert_equal "Charlotte Bronte", @charlotte_bronte.name
       assert_equal [], @charlotte_bronte.books
-    end  
+    end
+
+    def test_writes_a_book
+      # jane_eyre = Book.new({title: "Jane Eyre", publication_date: "October 16, 1847"})
+      # villette = Book.new({title: "Villette", publication_year: "1853"})
+      jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      assert_equal "Jane Eyre", jane_eyre.title
+      @charlotte_bronte.write("Villette", "1853")
+
+      # @charlotte_bronte.write(jane_eyre)
+      # @charlotte_bronte.write(villette)
+      assert_equal [jane_eyre, villette], @charlotte_bronte.books
+    end
+
 
 end

087fb95e1c0367d221e91d71699908f9e988e5c5
Jori Peterson <joripeterson@gmail.com>
2019-05-22 15:56:26 -0600

    Add unfinished write method

--- lib/author.rb
+++ lib/author.rb
@@ -1,18 +1,16 @@
 class Author
-  attr_reader :first_name, :last_name, :books
+  attr_reader :first_name, :last_name, :books, :name
 
   def initialize(info)
     @first_name = info[:first_name]
     @last_name = info[:last_name]
     @books = []
+    @name = "#{@first_name} #{@last_name}"
   end
 
-  def name
-    "#{@first_name} #{@last_name}"
-  end
-
-  def write(book)
-    @books << book
+  def write(title, publication_date)
+    @books << book = Book.new({:title => true, :publication_date => true})
+    # @books << Book.new(:title, :publication_year)
   end
 
 end

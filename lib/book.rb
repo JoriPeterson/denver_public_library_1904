d6053829622f520e04b6531f5ad414e271097e7b
Jori Peterson <joripeterson@gmail.com>
2019-05-22 15:55:49 -0600

    Refactor book for later methods

--- lib/book.rb
+++ lib/book.rb
@@ -1,19 +1,13 @@
 class Book
-  attr_reader :author_first_name, :author_last_name, :title, :publication_date
+  attr_reader :author_first_name, :author_last_name, :title, :publication_date, :author, :publication_year
 
   def initialize(details)
     @author_first_name = details[:author_first_name]
     @author_last_name = details[:author_last_name]
     @title = details[:title]
     @publication_date = details[:publication_date]
-  end
-
-  def author
-    "#{@author_first_name} #{@author_last_name}"
-  end
-
-  def publication_year
-    @publication_date[-4..-1]
+    @author = "#{@author_first_name} #{@author_last_name}"
+    @publication_year = details.values.last[-4..-1]
   end
 
 end

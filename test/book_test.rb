8fef4d5918824dd14f540cfd6b9ad681584c3739
Jori Peterson <joripeterson@gmail.com>
2019-05-22 14:12:05 -0600

    Add initial testing for book class

---
+++ test/book_test.rb
@@ -0,0 +1,21 @@
+require "mintest/autorun"
+require "mintest/pride"
+require "./lib/book"
+
+class BookTest < Mintest::Test
+
+  def setup
+    @book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+  end
+
+  def test_it_exists
+    assert_instance_of Book, @book
+  end
+
+  def test_it_has_attributes
+    assert_equal "Harper Lee", @book.author
+    assert_equal "To Kill a Mockingbird", @book.title
+    assert_equal "1960", @book.publication_year
+  end
+
+end
